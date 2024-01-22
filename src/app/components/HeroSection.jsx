267802df8219f10eb6a255f4b70578162ce592e0
Daniyal Shah <daniyalashah988@gmail.com>
2024-01-22 16:55:35 +0500

    About Page Pending

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,20 +1,47 @@
+"use client"
 import React from 'react'
 import Image from "next/image"
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                        Hello, I'm Daniyal
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+                            Hello, I'm{" "}
+                        </span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Daniyal',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Web Developer',
+                                1000,
+                                'Mobile/Desktop Developer',
+                                1000,
+                                'UI/UX Designer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
                     </h1>
-                    <p className='text-[#ADB7BE] text-lg lg:text-xl mb-6'>
+                    <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
                         THIS IS A DUMMY PARAGRAPH WHICH WILL BE UPDATED SOON
                     </p>
                     <div>
-                        <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black shadow-sm shadow-white'>Hire Me</button>
-                        <button className='px-6 py-3 rounded-full bg-sky-600 hover:bg-sky-500 text-ellipsis text-white border border-white mt-3 hover:shadow-md hover:shadow-blue-500/50'>Download Resume</button>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-200 text-white'>
+                            Hire Me
+                        </button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-ellipsis text-white mt-3'>
+                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                                Download Resume
+                            </span>
+                        </button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
