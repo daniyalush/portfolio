fe730995a09f8a6ea318416af1a9b3b3a5e07994
Daniyal Shah <daniyalashah988@gmail.com>
2024-01-23 13:28:38 +0500

    Updated!

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     return (
         <section className="text-black bg-[url('/images/about-image.png')]">
             <div className='md:grid md:grid-cols-2 gap-8 items-center px-4 py-8 xl:gap-16 sm:py-16 xl:px-16 mt-10' >
-                <h2 className='text-4xl font  font-bold text-black mb-4'>About Me</h2>
+                <h2 className='text-4xl font font-bold text-black mb-4 place-self-center'>About Me</h2>
                 <p className='text-base md:text-lg font-medium' >Experienced Full Stack Developer proficient in Next.js for both front and backend development.
                     Skilled in technologies such as Prisma, Node.js, Express.js, React, TailwindCSS. For databases I use MongoDB and PostgreSQL.
                     Well-versed in DevOps and project management, with additional knowledge of UI/UX design. Familiarity with Firebase and Supabase as well as Daisy UI and Next UI for enhanced user interface design.
