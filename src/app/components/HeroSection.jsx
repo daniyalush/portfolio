e2cd89156a62bd7d83c3d5347779ab66f8376151
Daniyal Shah <daniyalashah988@gmail.com>
2024-01-23 13:13:43 +0500

    Updated!

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
     return (
         <section>
             <div className="grid grid-cols-1 sm:grid-cols-12">
-                <div className="col-span-7 place-self-center text-center sm:text-left">
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+                <div className="col-span-8 place-self-center text-center sm:text-left">
+                    <h1 className="text-black mb-4 text-3xl sm:text-4xl lg:text-5xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 ">
                             Hello, I'm{" "}
                         </span>
                         <br />
@@ -20,7 +20,7 @@ const HeroSection = () => {
                                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                                 'Web Developer',
                                 1000,
-                                'Mobile/Desktop Developer',
+                                'Mobile App Developer',
                                 1000,
                                 'UI/UX Designer',
                                 1000
@@ -30,7 +30,7 @@ const HeroSection = () => {
                             repeat={Infinity}
                         />
                     </h1>
-                    <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
+                    <p className='text-[#000000] font-medium text-base sm:text-lg lg:text-xl mb-6'>
                         THIS IS A DUMMY PARAGRAPH WHICH WILL BE UPDATED SOON
                     </p>
                     <div>
@@ -44,13 +44,13 @@ const HeroSection = () => {
                         </button>
                     </div>
                 </div>
-                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-                    <div className="rounded-full bg-[#1a1919] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                        <Image src={"/images/hero-image2.png"}
+                <div className='col-span-3 place-self-center mt-4 lg:mt-0'>
+                    <div className="rounded-full bg-slate-200 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                        <Image src={"/images/hero-image.png"}
                             alt='Hero Image'
                             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                            width={300}
-                            height={300} />
+                            width={400}
+                            height={400} />
                     </div>
                 </div>
             </div>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     return (
         <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
-                <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
+                <Link href={"/"} className='animate-jump-in animate-once text-2xl md:text-4xl text-white font-semibold'>
                     LOGO
                 </Link >
                 <div className='mobile-menu block md:hidden'>
@@ -39,7 +39,7 @@ const Navbar = () => {
                     }
                 </div>
                 <div className='menu hidden md:block md:w-auto' id='navbar'>
-                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                    <ul className='flex p-6 md:p-0 md:flex-row md:space-x-10 mt-0'>
                         {navLinks.map((link, index) => (
                             <li key={index}>
                                 <NavLink href={link.path} title={link.title} />
